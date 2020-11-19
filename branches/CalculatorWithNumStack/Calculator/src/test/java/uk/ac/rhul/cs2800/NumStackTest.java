52a97dc111382ff9cdc012308c48105d2e223f53
zhac273 <zhac273@dbfe5ec5-a5db-4b25-93fc-61197ac41bee>
2020-11-19 18:35:53 +0000

    Changed the pop method to handle an EmptyStackException

--- branches/CalculatorWithNumStack/Calculator/src/test/java/uk/ac/rhul/cs2800/NumStackTest.java
+++ branches/CalculatorWithNumStack/Calculator/src/test/java/uk/ac/rhul/cs2800/NumStackTest.java
@@ -25,7 +25,7 @@ class NumStackTest {
   void testPush() {
     nstack.push(1);
   }
-  
+
   @Test
   // 2nd Test
   // To pass this test I added the pop method to the NumStack class.
@@ -33,7 +33,7 @@ class NumStackTest {
     nstack.push(1);
     assertEquals(nstack.pop(), 1, "Testing to see if the correct value is returned");
   }
-  
+
   @Test
   // 3rd Test
   // Did not need to make any changes to the method to pass this test
@@ -41,8 +41,24 @@ class NumStackTest {
     for (int x = 0; x <= 10; x++) {
       nstack.push(x);
     }
-    assertEquals(nstack.pop(), 10, 
-        "Test to see if the last value is popped");
+    assertEquals(nstack.pop(), 10, "Test to see if the last value is popped");
   }
+
+  @Test
+  // 4th Test
+  // To pass this test I added the isEmpty method and made a check to see if the stack size is 0
+  void testisEmpty() {
+    assertEquals(nstack.isEmpty(), true, "Test to see if the stack is empty");
+  }
+
+  @Test
+  // 5th Test
+  // To pass this test in the pop method I added a check that checks if the stack is empty
+  // and throws the EmptyStackException, otherwise it returns the value
+  void testException() {
+    assertThrows(EmptyStackException.class, () -> nstack.pop(),
+        "Test to see if the appropriate exception is thrown");
+  }
+
 
 }

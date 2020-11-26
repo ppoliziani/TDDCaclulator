1714a2c0ae6d4e94b65b0fbbea808c830a0434b5
zhac273 <zhac273@dbfe5ec5-a5db-4b25-93fc-61197ac41bee>
2020-11-26 18:18:44 +0000

    Added and tested check for empty strings to throw InvalidExpressionException

--- Calculator/src/test/java/uk/ac/rhul/cs2800/RevPolishCalcTest.java
+++ Calculator/src/test/java/uk/ac/rhul/cs2800/RevPolishCalcTest.java
@@ -28,8 +28,18 @@ class RevPolishCalcTest {
   // To pass this test I added a scanner that reads the
   // input of the string given to it and prints it to the console.
   void testReadingString() throws InvalidExpressionException {
-    assertEquals(revCalc.evaluate("7 2 +"), "72+",
+    // assertEquals(revCalc.evaluate("7 2 +"), "72+");
+    assertEquals(revCalc.evaluate("7 2 +"), 9,
         "Test to see if the string/expression passed is being read");
+  }
+
+  @Test
+  // 3rd Test
+  // To pass this test I added a check at the beginning to 
+  // check if the string is empty, throw the exception
+  void testNoInput() {
+    assertThrows(InvalidExpressionException.class, () -> revCalc.evaluate(""),
+        "Test to see if the invalidexpression exception is thrown");
   }
 
 }

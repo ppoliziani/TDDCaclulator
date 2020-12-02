5154ff3b71fd72331dcb9f499e9928968c629561
zhac273 <zhac273@dbfe5ec5-a5db-4b25-93fc-61197ac41bee>
2020-12-02 17:33:02 +0000

    Created the StandardCalc class that implements the calculator interface and created the evaluate method.

---
+++ Calculator/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
@@ -0,0 +1,25 @@
+package uk.ac.rhul.cs2800;
+
+/**
+ * Standard Calculator that uses the shunting yard algorithm to convert infix to postfix
+ * expressions.
+ * 
+ * @author Philip
+ *
+ */
+public class StandardCalc implements CalculatorInterface {
+
+  /**
+   * Evaluates the expression.
+   * 
+   * @param what expression to be converted and evaluated
+   * @return result of expression
+   */
+  public float evaluate(String what) {
+
+    float result = 20;
+
+    return result;
+  }
+
+}

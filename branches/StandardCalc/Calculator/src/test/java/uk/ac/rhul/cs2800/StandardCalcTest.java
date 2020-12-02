dc73167cc61ad640838e6bdf2c6ed170c54ceb78
zhac273 <zhac273@dbfe5ec5-a5db-4b25-93fc-61197ac41bee>
2020-12-02 18:09:35 +0000

    Added scanner to read the string input character by character.

--- branches/StandardCalc/Calculator/src/test/java/uk/ac/rhul/cs2800/StandardCalcTest.java
+++ branches/StandardCalc/Calculator/src/test/java/uk/ac/rhul/cs2800/StandardCalcTest.java
@@ -16,9 +16,21 @@ class StandardCalcTest {
   }
 
   @Test
-  void test() {
+  // 1st Test
+  // To pass this test I created the StandardCalc class that implements the calculator interface and
+  // implements its evaluate method
+  void testStart() {
     assertEquals(standCalc.evaluate(""), 20,
         "Test to see if evaluate method works and returns a float");
   }
 
+  @Test
+  // 2nd Test
+  // To pass this test I created a new scanner on the string passed to the evaluate method using a
+  // while loop to check if the input still has a next character and an if statement checking if
+  // that character is a float
+  void testScanner() {
+    assertEquals(standCalc.evaluate("7 2 5 + /"), 20,
+        "Expecting to see the scanner print only the numbers");
+  }
 }

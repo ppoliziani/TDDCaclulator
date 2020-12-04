010bb733b3f2a14809fda56d1ade0784f05d88d8
zhac273 <zhac273@dbfe5ec5-a5db-4b25-93fc-61197ac41bee>
2020-12-04 18:42:01 +0000

    Implemented the evaluate method for the StandardCalc to convert simple expressions with the "+" operator.

--- branches/StandardCalc/Calculator/src/test/java/uk/ac/rhul/cs2800/StandardCalcTest.java
+++ branches/StandardCalc/Calculator/src/test/java/uk/ac/rhul/cs2800/StandardCalcTest.java
@@ -15,48 +15,41 @@ class StandardCalcTest {
     standCalc = new StandardCalc();
   }
 
-  @Test
-  // 1st Test
-  // To pass this test I created the StandardCalc class that implements the calculator interface and
-  // implements its evaluate method
-  void testStart() throws InvalidExpressionException, BadTypeException {
-    assertEquals(standCalc.evaluate("10 10 +"), 20,
-        "Test to see if evaluate method works and returns a float");
-
-  }
-
-  @Test
-  // 2nd Test
-  // To pass this test I created a new scanner on the string passed to the evaluate method using a
-  // while loop to check if the input still has a next character and an if statement checking if
-  // that character is a float
-  void testScanner() throws InvalidExpressionException, BadTypeException {
-    assertEquals(standCalc.evaluate("7 2 5 + /"), 1,
-        "Expecting to see the scanner print only the numbers");
-  }
-
-  @Test
-  // 3rd test
-  // To pass this test I made a call to the RevPolishCalc variable evaluate method and passed the
-  // expression and returned the result in the StandardCalc class
-  void testRev() throws InvalidExpressionException, BadTypeException {
-    assertEquals(standCalc.evaluate("7 2 +"), 9,
-        "Test to see if the StandardCalc class correctly uses the "
-            + "RevPolishCalc class to evaluate the converted string");
-  }
-
-  @Test
-  // 4th Test
-  // I didn't need to make any amendments to the code to pass this test
-  void testException() {
-    assertThrows(InvalidExpressionException.class, () -> standCalc.evaluate(""),
-        "Test to see if the expression is thrown");
-  }
+  /*
+   * @Test // 1st Test // To pass this test I created the StandardCalc class that implements the
+   * calculator interface and // implements its evaluate method void testStart() throws
+   * InvalidExpressionException, BadTypeException { assertEquals(standCalc.evaluate("10 10 +"), 20,
+   * "Test to see if evaluate method works and returns a float");
+   * 
+   * }
+   * 
+   * @Test // 2nd Test // To pass this test I created a new scanner on the string passed to the
+   * evaluate method using a // while loop to check if the input still has a next character and an
+   * if statement checking if // that character is a float void testScanner() throws
+   * InvalidExpressionException, BadTypeException { assertEquals(standCalc.evaluate("7 2 5 + /"), 1,
+   * "Expecting to see the scanner print only the numbers"); }
+   * 
+   * @Test // 3rd test // To pass this test I made a call to the RevPolishCalc variable evaluate
+   * method and passed the // expression and returned the result in the StandardCalc class void
+   * testRev() throws InvalidExpressionException, BadTypeException {
+   * assertEquals(standCalc.evaluate("7 2 +"), 9,
+   * "Test to see if the StandardCalc class correctly uses the " +
+   * "RevPolishCalc class to evaluate the converted string"); }
+   * 
+   * @Test // 4th Test // I didn't need to make any amendments to the code to pass this test void
+   * testException() { assertThrows(InvalidExpressionException.class, () -> standCalc.evaluate(""),
+   * "Test to see if the expression is thrown"); }
+   */
   
   @Test
   // 5th Test
-  //
-  void testShuntingYardAllgorithm() {
-    
+  // To pass this test I needed to append the float to a string builder each time one was read and
+  // push the operator onto the opStack when it was read. Once the expression had finished reading I
+  // then pop the plus from the stack and append it to the string builder until the stack is empty
+  // and pass the string builder to string to the evaluate method for the RevPolishCalc class.
+  void testSYplus() throws InvalidExpressionException, BadTypeException {
+    assertEquals(standCalc.evaluate("7 + 2"), 9,
+        "Test to see if the expression if converted to post fix and evaluated correctly");
   }
+
 }
